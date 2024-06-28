f28849f925d9ff68e43ae7c67ff9176a5ade1a73
Alexis Merino <alexismerino49@gmail.com>
2024-06-28 04:00:54 -0500

    added Navbar and tooglecolormode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {React, useState} from 'react';
+import PropTypes from 'prop-types';
+import Navbar from './components/Navbar'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState('light');
+  
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Navbar mode={mode} toggleColorMode={toggleColorMode} />
+
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,219 @@
 import { useState } from "react";
-import {Link} from "react-router-dom";
-import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg"
+
+// importing material ui components
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';    
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
+import ToggleColorMode from './ToggleColorMode';
+import PropTypes from 'prop-types';
+
+const logoStyle = {
+    width: '50px',
+    height: 'auto',
+    cursor: 'pointer',
+};
 
 
+const Navbar = ({ mode, toggleColorMode }) => {
+    const [open, setOpen] = useState(false);
 
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
 
+    const scrollToSection = (sectionId) => {
+        const sectionElement = document.getElementById(sectionId);
+        const offset = 128;
+        if (sectionElement) {
+            const targetScroll = sectionElement.offsetTop - offset;
+            sectionElement.scrollIntoView({ behavior: 'smooth' });
+            window.scrollTo({
+                top: targetScroll,
+                behavior: 'smooth',
+            });
+            setOpen(false);
+        }
+    };
 
-const Navbar = () => {
-
-return (
-    <>
-       <p>Navbar</p>     
-    </>
-)
-
+    return (
+        <div>
+            <AppBar
+                position="fixed"
+                sx={{
+                    boxShadow: 0,
+                    bgcolor: 'transparent',
+                    backgroundImage: 'none',
+                    mt: 2,
+                }}
+            >
+                <Container maxWidth="lg">
+                    <Toolbar
+                        variant="regular"
+                        sx={(theme) => ({
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            flexShrink: 0,
+                            borderRadius: '999px',
+                            bgcolor:
+                                theme.palette.mode === 'light'
+                                    ? 'rgba(255, 255, 255, 0.4)'
+                                    : 'rgba(0, 0, 0, 0.4)',
+                            backdropFilter: 'blur(24px)',
+                            maxHeight: 40,
+                            border: '1px solid',
+                            borderColor: 'divider',
+                            boxShadow:
+                                theme.palette.mode === 'light'
+                                    ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
+                                    : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+                        })}
+                    >
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                alignItems: 'center',
+                                ml: '-18px',
+                                px: 2,
+                            }}
+                        >
+                            <img
+                                src={logo}
+                                style={logoStyle}
+                                alt="logo of FT"
+                            />
+                            <Box sx={{ display: { xs: 'none', md: 'flex' }, ml:5  }}>
+                                
+                                <MenuItem   >
+                                    <Typography variant="body2" color="text.primary">
+                                        Tracker
+                                    </Typography>
+                                </MenuItem>
+                                <MenuItem
+                                    
+                                >
+                                    <Typography variant="body2" color="text.primary">
+                                        Highlights
+                                    </Typography>
+                                </MenuItem>
+                                
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: { xs: 'none', md: 'flex' },
+                                gap: 0.5,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+                            <Button
+                                color="primary"
+                                variant="text"
+                                size="small"
+                                component="a"
+                                href="/material-ui/getting-started/templates/sign-in/"
+                                target="_blank"
+                            >
+                                Sign in
+                            </Button>
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                size="small"
+                                component="a"
+                                href="/material-ui/getting-started/templates/sign-up/"
+                                target="_blank"
+                            >
+                                Sign up
+                            </Button>
+                        </Box>
+                        <Box sx={{ display: { sm: '', md: 'none' } }}>
+                            <Button
+                                variant="text"
+                                color="primary"
+                                aria-label="menu"
+                                onClick={toggleDrawer(true)}
+                                sx={{ minWidth: '30px', p: '4px' }}
+                            >
+                                <MenuIcon />
+                            </Button>
+                            <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                                <Box
+                                    sx={{
+                                        minWidth: '60dvw',
+                                        p: 2,
+                                        backgroundColor: 'background.paper',
+                                        flexGrow: 1,
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'end',
+                                            flexGrow: 1,
+                                        }}
+                                    >
+                                        <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+                                    </Box>
+                                    <MenuItem  >
+                                        Tracker
+                                    </MenuItem>
+                                    <MenuItem >
+                                        Profile
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            component="a"
+                                            href="../pages/signup"
+                                            target="_blank"
+                                            sx={{ width: '100%' }}
+                                        >
+                                            Sign up
+                                        </Button>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="outlined"
+                                            component="a"
+                                            href="../pages/login"
+                                            target="_blank"
+                                            sx={{ width: '100%' }}
+                                        >
+                                            Login
+                                        </Button>
+                                    </MenuItem>
+                                </Box>
+                            </Drawer>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
+    );
 }
+
+Navbar.propTypes = {
+    mode: PropTypes.oneOf(['dark', 'light']).isRequired,
+    toggleColorMode: PropTypes.func.isRequired,
+};
+
+
 
 
 export default Navbar;
