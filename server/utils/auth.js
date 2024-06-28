982a6d82934603bf054bbe972ea4f7c165228991
Alexis Merino <alexismerino49@gmail.com>
2024-06-27 19:14:39 -0500

    deleted the response for invalid token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -34,7 +34,6 @@ module.exports = {
             req.user = data;
         } catch {
             console.log('Invalid token');
-            return res.status(400).json({ message: 'invalid token!' });
         }
 
         // return the token 
