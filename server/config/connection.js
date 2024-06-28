a37567e951c8a87cb33fdb4b3c3b509d356c4bc8
Josh Lee <joshua.huggins@sbcglobal.net>
2024-06-27 21:46:45 -0500

    added the seeds to find and fix errors with resolvers/typedefs, all issues are fixed right now

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 // come back to put in the actual server route
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/finance-tracking');
 
 module.exports = mongoose.connection;
