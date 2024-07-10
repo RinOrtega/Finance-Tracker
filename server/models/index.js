2baba82dafd3a1cb4c2c66119e785cdcb8496e11
Josh Lee <joshua.huggins@sbcglobal.net>
2024-07-09 21:16:36 -0500

    added category seeds

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const User = require('./User');
 const Transaction = require('./Transaction');
 const Category = require('./Category')
 
-module.exports = {User, Transaction};
+module.exports = {User, Transaction, Category};
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
-const { User, Transaction } = require('../models');
+const { User, Transaction, Category } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const transactionSeeds = require('./transactionSeeds.json');
+const categorySeeds = require('./categorySeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async() => {
@@ -10,9 +11,13 @@ db.once('open', async() => {
 
         await cleanDB('User', 'users');
 
+        await cleanDB('Category', 'category')
+
         await User.create(userSeeds);
 
         await Transaction.create(transactionSeeds);
+
+        await Category.create(categorySeeds);
     } catch (err) {
         console.error(err);
         process.exit(1);
